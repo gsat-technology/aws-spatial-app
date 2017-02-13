857637fb1ae16090bdeeed931e70157d88468d3d
gsat-technology <gsat.technology@gmail.com>
2017-02-13 17:55:50 +1100

    fix apt-get issue

--- ec2-conf/install-nginx.sh
+++ ec2-conf/install-nginx.sh
@@ -2,10 +2,10 @@
 
 FQDN=$1
 
-apt-get install nginx
+apt-get install -y nginx
 service nginx start
-apt-get install letsencrypt
+apt-get install -y letsencrypt
 letsencrypt certonly -a webroot --webroot-path=/var/www/html -d $FQDN
 
 #replace default host config with pre-configured file and dynamically configure server_name
-sed -e s/{SERVER_NAME}/spatial2.gsat.technology/g ./spatial/ec2-conf/default > /etc/nginx/sites-enabled/default
+sed -e s/{SERVER_NAME}/$FQDN/g ./spatial/ec2-conf/default > /etc/nginx/sites-enabled/default

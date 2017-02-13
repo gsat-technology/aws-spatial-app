bdfb458cb6d71f964a5a6c6d5c0a7a26fa9aa017
gsat-technology <gsat.technology@gmail.com>
2017-02-13 20:08:10 +1100

    nginx conf working now

--- ec2-conf/install-nginx.sh
+++ ec2-conf/install-nginx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#installs nginx and letsencrypt
+#configures SSL first then switches in an nginx config file replaces FQDN in the file
+
 FQDN=$1
 EMAIL$2
 
@@ -17,4 +20,5 @@ EOF
 letsencrypt certonly --config /cli.ini -a webroot --webroot-path=/var/www/html -d $FQDN
 
 #replace default host config with pre-configured file and dynamically configure server_name
-sed -e s/{SERVER_NAME}/$FQDN/g ./spatial/ec2-conf/default > /etc/nginx/sites-enabled/default
+sed -e s/{FQDN}/$FQDN/g ./spatial/ec2-conf/default > /etc/nginx/sites-enabled/default
+service nginx restart

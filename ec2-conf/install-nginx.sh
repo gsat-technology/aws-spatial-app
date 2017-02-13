faf10aa27b3868c9ce1b112a54fdcf4d44e4cb51
gsat-technology <gsat.technology@gmail.com>
2017-02-13 18:11:50 +1100

    adds unattended params for letsencrypt

--- ec2-conf/install-nginx.sh
+++ ec2-conf/install-nginx.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 FQDN=$1
+EMAIL$2
 
 apt-get install -y nginx
 service nginx start
 apt-get install -y letsencrypt
-letsencrypt certonly -a webroot --webroot-path=/var/www/html -d $FQDN
+
+#unattended params
+cat >/cli.ini <<EOF
+  email=$EMAIL
+  agree-tos=True
+  text=True
+EOF
+
+letsencrypt certonly --config /cli.ini -a webroot --webroot-path=/var/www/html -d $FQDN
 
 #replace default host config with pre-configured file and dynamically configure server_name
 sed -e s/{SERVER_NAME}/$FQDN/g ./spatial/ec2-conf/default > /etc/nginx/sites-enabled/default
